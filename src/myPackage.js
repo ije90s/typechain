356a1709499a41241dda295656650e501c3a58a5
ije90s <idgocm@gmail.com>
2022-12-22 18:38:59 +0900

    JsDocs

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,21 @@
+// @ts-check
+
+/**
+ * 프로젝트 초기화
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url 
+ * @returns {boolean}
+ */
 export function init(config){
     return true;
 }
+
+/**
+ * 프로그램 종료
+ * @param {number} code 
+ * @returns {number}
+ */
 export function exit(code){
     return code+1;
 }
